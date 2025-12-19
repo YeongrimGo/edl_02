0de1056eb8895fe020f2f2905a4f80b0d3267838
hyenyy <ww3700@pusan.ac.kr>
2025-12-19 17:49:22 +0900

    전체코드수정

--- project_test/Libraries/ALARM/alarm.c
+++ project_test/Libraries/ALARM/alarm.c
@@ -1,6 +1,6 @@
 #include "alarm.h"
 #include "lcd.h"
-#include "stm32f10x_gpio.h"
+#include "inc/hw_config.h"
 #include "stm32f10x_tim.h"
 #include <stdio.h>
 
@@ -9,23 +9,23 @@ static volatile uint32_t countdown_seconds = 0;
 static volatile uint32_t elapsed_seconds = 0;
 
 volatile uint32_t* p_countdown_seconds = &countdown_seconds;
-volatile uint32_t* p_elapsed_seconds = &elapsed_seconds;
-volatile AlarmState* p_alarm_state = &alarm_state;
-
-// 수동 부저용 지연 함수 (주파수 생성용)
-void Beep_Delay(volatile uint32_t count) { while (count--); }
+volatile uint32_t* p_elapsed_seconds   = &elapsed_seconds;
+volatile AlarmState* p_alarm_state     = &alarm_state;
 
 void Alarm_Init(void) {
+    // TIM2 설정 (1초 인터럽트)
     TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-    // 1초 간격 인터럽트 설정
-    TIM_TimeBaseStructure.TIM_Prescaler = 7200 - 1;
-    TIM_TimeBaseStructure.TIM_Period = 10000 - 1;
+
+    TIM_TimeBaseStructure.TIM_Prescaler     = 7200 - 1;
+    TIM_TimeBaseStructure.TIM_Period        = 10000 - 1;
     TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseStructure.TIM_CounterMode   = TIM_CounterMode_Up;
+
     TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
     TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
-    
-    GPIO_ResetBits(GPIOA, GPIO_Pin_3); // 소리 OFF
+
+    // 부저는 기본 OFF
+    BUZZER_Stop();
 }
 
 void Alarm_Start(uint16_t seconds) {
@@ -33,7 +33,8 @@ void Alarm_Start(uint16_t seconds) {
         *p_countdown_seconds = seconds;
         *p_alarm_state = STATE_COUNTDOWN;
         *p_elapsed_seconds = 0;
-        GPIO_ResetBits(GPIOA, GPIO_Pin_3); 
+
+        BUZZER_Stop();
         LCD_Clear(WHITE);
         TIM_Cmd(TIM2, ENABLE);
     }
@@ -42,50 +43,52 @@ void Alarm_Start(uint16_t seconds) {
 void Alarm_Process(void) {
     char lcd_buffer[30];
     static int32_t last_displayed_second = -1;
+    static uint8_t buzzer_started = 0;
 
     switch (*p_alarm_state) {
-        case STATE_COUNTDOWN:
-            if (last_displayed_second != (int32_t)*p_countdown_seconds) {
-                last_displayed_second = *p_countdown_seconds;
-                sprintf(lcd_buffer, "Time: %02d:%02d", (int)(*p_countdown_seconds/60), (int)(*p_countdown_seconds%60));
-                LCD_ShowString(40, 130, (u8*)lcd_buffer, BLUE, WHITE);
-            }
-            break;
+    case STATE_COUNTDOWN:
+        buzzer_started = 0;
+        if (last_displayed_second != (int32_t)*p_countdown_seconds) {
+            last_displayed_second = (int32_t)*p_countdown_seconds;
+            sprintf(lcd_buffer, "Time: %02d:%02d",
+                    (int)(*p_countdown_seconds / 60),
+                    (int)(*p_countdown_seconds % 60));
+            LCD_ShowString(40, 130, (u8*)lcd_buffer, BLUE, WHITE);
+        }
+        break;
 
-        case STATE_ALARM_ACTIVE:
-            // [중요] 내장 부저(수동형) 구동 로직
-            // LCD를 그리느라 루프가 느려지기 전에 여기서 주파수를 흔들어 줌
-            for(int i = 0; i < 200; i++) {
-                GPIO_SetBits(GPIOA, GPIO_Pin_3);   // ON
-                Beep_Delay(800);                   // 주파수 조절 (값이 작을수록 고음)
-                GPIO_ResetBits(GPIOA, GPIO_Pin_3); // OFF
-                Beep_Delay(800);
-            }
+    case STATE_ALARM_ACTIVE:
+        if (!buzzer_started) {
+            buzzer_started = 1;
+            BUZZER_Start(2000); // 2kHz (필요하면 1000~4000 사이로 조절)
+            LCD_Clear(RED);
+            LCD_ShowString(40, 100, (u8*)"WAKE UP!", WHITE, RED);
+        }
+        break;
 
-            if (*p_elapsed_seconds == 1) { 
-                LCD_Clear(RED);
-                LCD_ShowString(40, 100, (u8*)"WAKE UP!", WHITE, RED);
-            }
-            break;
+    case STATE_ALARM_STOPPED:
+        buzzer_started = 0;
+        BUZZER_Stop();
+        break;
 
-        case STATE_ALARM_STOPPED:
-            GPIO_ResetBits(GPIOA, GPIO_Pin_3); // 소리 중지
-            break;
-
-        default:
-            GPIO_ResetBits(GPIOA, GPIO_Pin_3);
-            break;
+    default:
+        buzzer_started = 0;
+        BUZZER_Stop();
+        break;
     }
 }
 
 AlarmState Alarm_GetState(void) { return *p_alarm_state; }
 uint32_t Alarm_GetElapsedSeconds(void) { return *p_elapsed_seconds; }
+
 void Alarm_Reset(void) {
     *p_alarm_state = STATE_IDLE;
     *p_elapsed_seconds = 0;
     *p_countdown_seconds = 0;
-    GPIO_ResetBits(GPIOA, GPIO_Pin_3); 
+
+    BUZZER_Stop();
     TIM_Cmd(TIM2, DISABLE);
+
     LCD_Clear(WHITE);
     LCD_ShowString(40, 100, (u8*)"Alarm Idle", BLUE, WHITE);
 }
