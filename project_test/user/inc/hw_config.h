7d9bddc1b6dd9b1050afdfb2fdee638aea70737e
hyenyy <ww3700@pusan.ac.kr>
2025-12-19 18:03:48 +0900

    블루투스 되던때로 롤백

--- project_test/user/inc/hw_config.h
+++ project_test/user/inc/hw_config.h
@@ -1,22 +1,14 @@
-#ifndef __HW_CONFIG_H__
-#define __HW_CONFIG_H__
+#ifndef __HW_CONFIG_H
+#define __HW_CONFIG_H
 
 #include "stm32f10x.h"
-#include <stdint.h>
 
 void RCC_Configure(void);
 void GPIO_Configure(void);
 void NVIC_Configure(void);
+void USART2_Init(void);
+void USART2_SendString(const char* str);
 void DMA_Configure(void);
 void ADC_Configure(void);
 
-void USART1_Init(void);
-void USART2_Init(void);
-void USART2_SendString(const char* str);
-
-/* Buzzer on PC6 (TIM3_CH1 Full Remap), Low-Trigger */
-void BUZZER_Configure(void);
-void BUZZER_Start(uint16_t freq_hz);
-void BUZZER_Stop(void);
-
-#endif
+#endif /* __HW_CONFIG_H */
