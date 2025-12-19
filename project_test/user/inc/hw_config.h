0c214a2cf79ed2d626ce813de21dc3625e48a8bf
hyenyy <ww3700@pusan.ac.kr>
2025-12-19 17:57:57 +0900

    헤더파일

--- project_test/user/inc/hw_config.h
+++ project_test/user/inc/hw_config.h
@@ -1,14 +1,22 @@
-#ifndef __HW_CONFIG_H
-#define __HW_CONFIG_H
+#ifndef __HW_CONFIG_H__
+#define __HW_CONFIG_H__
 
 #include "stm32f10x.h"
+#include <stdint.h>
 
 void RCC_Configure(void);
 void GPIO_Configure(void);
 void NVIC_Configure(void);
-void USART2_Init(void);
-void USART2_SendString(const char* str);
 void DMA_Configure(void);
 void ADC_Configure(void);
 
-#endif /* __HW_CONFIG_H */
+void USART1_Init(void);
+void USART2_Init(void);
+void USART2_SendString(const char* str);
+
+/* Buzzer on PC6 (TIM3_CH1 Full Remap), Low-Trigger */
+void BUZZER_Configure(void);
+void BUZZER_Start(uint16_t freq_hz);
+void BUZZER_Stop(void);
+
+#endif
