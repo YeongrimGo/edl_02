b8df849682bcf27e26e5d08093b7563441061c14
myeowon <myw7931@gmail.com>
2025-12-21 21:58:28 +0900

    Revert "LCD 관련 수정사항"
    This reverts commit 1317a5c3b87cabea32d0ee97ee5b0cdc328fbd53.

--- project_test/user/main.c
+++ project_test/user/main.c
@@ -26,12 +26,10 @@ int main(void) {
     USART1_Init();
     USART2_Init();
     Alarm_Init();
-
-    // [수정] 초기 상태를 '블루투스 연결 대기'로 설정
     Alarm_Reset();
-    *p_alarm_state = STATE_WAIT_BLUETOOTH; // 강제 설정
 
-    // 주의: 초기 메시지는 PA0 버튼을 눌러 STATE_IDLE로 진입할 때 전송됨.
+    // [요청] 블루투스 연결 완료 메시지 (영어)
+    USART2_SendString("\r\nBluetooth Connected. Please send the alarm time in seconds.\r\n");
 
     while (1) {
         Alarm_Process();
@@ -41,14 +39,14 @@ int main(void) {
             char time_str[20];
             char report[80];
 
-            // [수정] 00:00 포맷 적용
+            // [요청] 00시 00분 00초 형식으로 변환
             Time_Format(final_time, time_str);
 
             sprintf(report, "\r\n[STOP] Duration: %s\r\n", time_str);
             USART2_SendString(report);
 
             for(volatile int i=0; i<5000000; i++);
-            Alarm_Reset(); // 리셋 후에는 STATE_IDLE 상태가 되어 바로 다음 알람 입력 대기
+            Alarm_Reset();
         }
     }
 }
