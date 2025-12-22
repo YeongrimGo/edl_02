dbae6d50eba538bf32b12b3582cb7cc0cc01ff29
myeowon <myw7931@gmail.com>
2025-12-23 01:22:53 +0900

    Revert "블루투스 연결 후 숫자 대기 상태 추가"
    This reverts commit fc098f87d329df409b00dc5e5098e4f489806753.

--- project_test/user/stm32f10x_it.c
+++ project_test/user/stm32f10x_it.c
@@ -3,7 +3,7 @@
 #include "stm32f10x_exti.h"
 #include "stm32f10x_tim.h"
 #include "stm32f10x_adc.h"
-#include "stm32f10x_gpio.h"
+#include "stm32f10x_gpio.h" // GPIO 사용을 위해 추가
 #include "alarm.h"
 #include "inc/hw_config.h"
 #include <string.h>
@@ -13,21 +13,89 @@ extern volatile uint32_t* p_countdown_seconds;
 extern volatile uint32_t* p_elapsed_seconds;
 extern volatile AlarmState* p_alarm_state;
 
+// hw_config.c에 정의된 모터 제어 변수 가져오기
 extern volatile uint16_t motor_speed;
 extern volatile int motor_state;
 
 char rx_buffer[50];
 uint8_t rx_index = 0;
 
+// [추가] TIM3 인터럽트 핸들러: 여기서 980 PWM을 구현함
 void TIM3_IRQHandler(void) {
     static uint16_t pwm_count = 0;
+
     if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET) {
         TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+
+        // 0~1000 카운트 반복
         pwm_count++;
         if (pwm_count >= 1000) pwm_count = 0;
 
-        // 간단한 모터 PWM 제어 예시 (hw_config 참조 필요)
-        // 여기서는 인터럽트 플래그 클리어만 확실히 처리
+        // PWM 제어 로직 (Count가 Speed보다 작을 때만 ON)
+        if (pwm_count < motor_speed) {
+            switch (motor_state) {
+                case 1: // Forward
+                    // Left: IN1(PC10)=0, IN2(PC0)=1
+                    GPIO_ResetBits(GPIOC, GPIO_Pin_10); GPIO_SetBits(GPIOC, GPIO_Pin_0);
+                    // Right: IN3(PB7)=0, IN4(PB8)=1
+                    GPIO_ResetBits(GPIOB, GPIO_Pin_7); GPIO_SetBits(GPIOB, GPIO_Pin_8);
+                    break;
+                case 2: // Backward
+                    // Left: IN1(PC10)=1, IN2(PC0)=0
+                    GPIO_SetBits(GPIOC, GPIO_Pin_10); GPIO_ResetBits(GPIOC, GPIO_Pin_0);
+                    // Right: IN3(PB7)=1, IN4(PB8)=0
+                    GPIO_SetBits(GPIOB, GPIO_Pin_7); GPIO_ResetBits(GPIOB, GPIO_Pin_8);
+                    break;
+                case 3: // Turn Left
+                    // Left Backward
+                    GPIO_SetBits(GPIOC, GPIO_Pin_10); GPIO_ResetBits(GPIOC, GPIO_Pin_0);
+                    // Right Stop
+                    GPIO_ResetBits(GPIOB, GPIO_Pin_7); GPIO_ResetBits(GPIOB, GPIO_Pin_8);
+                    break;
+                case 4: // Turn Right
+                    // Left Stop
+                    GPIO_ResetBits(GPIOC, GPIO_Pin_10); GPIO_ResetBits(GPIOC, GPIO_Pin_0);
+                    // Right Backward
+                    GPIO_SetBits(GPIOB, GPIO_Pin_7); GPIO_ResetBits(GPIOB, GPIO_Pin_8);
+                    break;
+                case 0: // Stop
+                default:
+                    GPIO_ResetBits(GPIOC, GPIO_Pin_10 | GPIO_Pin_0);
+                    GPIO_ResetBits(GPIOB, GPIO_Pin_7 | GPIO_Pin_8);
+                    break;
+            }
+        } else {
+            // PWM Off 기간: 모든 핀 끄기
+            GPIO_ResetBits(GPIOC, GPIO_Pin_10 | GPIO_Pin_0);
+            GPIO_ResetBits(GPIOB, GPIO_Pin_7 | GPIO_Pin_8);
+        }
+    }
+}
+
+void TIM2_IRQHandler(void) {
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) {
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+
+        if (*p_alarm_state == STATE_COUNTDOWN) {
+            if (*p_countdown_seconds > 0) {
+                (*p_countdown_seconds)--;
+            }
+            if (*p_countdown_seconds == 0) {
+                *p_alarm_state = STATE_ALARM_ACTIVE;
+            }
+        } else if (*p_alarm_state == STATE_ALARM_ACTIVE || *p_alarm_state == STATE_WAIT_FOR_RAIN) {
+            (*p_elapsed_seconds)++;
+        }
+    }
+}
+
+void EXTI0_IRQHandler(void) {
+    if (EXTI_GetITStatus(EXTI_Line0) != RESET) {
+        if (*p_alarm_state == STATE_ALARM_ACTIVE || *p_alarm_state == STATE_WAIT_FOR_RAIN) {
+            *p_alarm_state = STATE_ALARM_STOPPED;
+            TIM_Cmd(TIM2, DISABLE);
+        }
+        EXTI_ClearITPendingBit(EXTI_Line0);
     }
 }
 
@@ -40,37 +108,23 @@ void USART1_IRQHandler(void) {
     }
 }
 
-// [핵심] 블루투스 수신 핸들러
 void USART2_IRQHandler(void) {
     if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET) {
         uint16_t word = USART_ReceiveData(USART2);
-        USART_SendData(USART1, word); // 디버깅용
+        USART_SendData(USART1, word);
 
         if (rx_index < sizeof(rx_buffer) - 1) {
-            // 엔터(\n, \r)를 만나면 명령 처리
             if (word == '\n' || word == '\r') {
                 if (rx_index > 0) {
                     rx_buffer[rx_index] = '\0';
-
-                    // 1. 숫자인지 확인 (알람 시간 설정)
                     int received_val = atoi(rx_buffer);
 
                     if (received_val > 0) {
-                        // 숫자면 알람 카운트다운 시작
                         Alarm_Start((uint16_t)received_val);
-                    }
-                    else {
-                        // 2. 숫자가 아니면 (예: "ok", "connect", "c") -> 연결 상태로 전환
-                        // 현재 IDLE 상태일 때만 전환
-                        if (Alarm_GetState() == STATE_IDLE) {
-                            Alarm_Set_BT_Connected();
-                        }
                     }
                     rx_index = 0;
                 }
-            }
-            // 데이터 버퍼에 저장
-            else {
+            } else if (word >= '0' && word <= '9') {
                 rx_buffer[rx_index++] = (char)word;
             }
         } else {
@@ -82,10 +136,15 @@ void USART2_IRQHandler(void) {
 
 void EXTI1_IRQHandler(void) {
     if (EXTI_GetITStatus(EXTI_Line1) != RESET) {
-        // 빗물 감지 대기 상태로 전환 (알람 울릴 때 터치 시)
         if (*p_alarm_state == STATE_ALARM_ACTIVE) {
             *p_alarm_state = STATE_WAIT_FOR_RAIN;
+            Sensor_Mode_WaitRain();
+            USART2_SendString("\r\nTouch Detected! Waiting for rain...\r\n");
         }
         EXTI_ClearITPendingBit(EXTI_Line1);
     }
+}
+
+void ADC1_2_IRQHandler(void) {
+    ADC_ClearITPendingBit(ADC1, ADC_IT_AWD);
 }
